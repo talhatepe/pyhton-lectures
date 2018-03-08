16b15181fa6a2bf382280fd33fe5b7e8da7a7498
Talha Tepe <talhaatepe@hotmail.com>
2018-03-09 00:27:50 +0300

    Homework - Week 2

---
+++ week2/hw1-2.py
@@ -0,0 +1,14 @@
+import random
+
+arr = [None] * 100
+for i in range(1, 100):
+    arr[i] = i
+    arr2 = i
+
+arr[0] = random.randint(1, 99)
+print(arr)
+
+random.shuffle(arr)
+
+randoms = [x for n, x in enumerate(arr) if x in arr[:n]]
+print ("Duplicate Num: ", randoms)
