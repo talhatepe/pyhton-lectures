6a0e1654f43d4cc9d20ddf0a1ecf12284a754009
Talha Tepe <talhaatepe@hotmail.com>
2018-03-09 17:10:24 +0300

    Update hw3.py

--- week2/hw3.py
+++ week2/hw3.py
@@ -1,6 +1,12 @@
 while True:
-    str = input(": ")
+    str = input(": ").lower()
     arr = []
-    arr += str
-    notDubStr = [i for j , i in enumerate(arr) if i not in arr[:j]]
-    print (''.join(notDubStr))
+    for i in str:
+        bool = False
+        for j in arr:
+            if i == j:
+                bool = True
+                break
+        if bool == False:
+            arr.append(i) 
+    print(''.join(arr))
