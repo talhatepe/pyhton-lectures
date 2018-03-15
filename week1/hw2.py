5db182d0923b8f04ec01457143a1c170175a1f40
Talha Tepe <talhaatepe@hotmail.com>
2018-03-16 01:39:35 +0300

    Upload Last Change

---
+++ week1/hw2.py
@@ -0,0 +1,8 @@
+x=0
+y=1
+sayi = int(input("Sayı Giriniz: "))
+for i in range(0,sayi):
+    print(y)
+    z=x
+    x=y
+    y=z+y
---
+++ week3/hw1.py
@@ -0,0 +1,20 @@
+input = input(": ")
+def rev(str):
+    arr = []
+    arr += str
+    leng = len(arr)
+    ave = leng / 2
+    single = True
+    if leng % 2 == 0:
+        single = False
+    if single:
+        ave = round(ave)
+    ave2 = int(ave)
+    for i in range(0, ave2):
+        tmp = arr[i]
+        arr[i] = arr[0 - (i + 1)]
+        arr[0 - (i + 1)] = tmp
+    return arr
+
+
+print (''.join(rev(input)))
