d12684ae15811882c80e4bec105398923a587278
Talha Tepe <talhaatepe@hotmail.com>
2018-03-20 16:49:36 +0300

    Update hw1.py

--- week2/hw1.py
+++ week2/hw1.py
@@ -1,15 +1,18 @@
 import random
 
 arr = [None] * 100
-for i in range (1,100):
+for i in range(1, 100):
     arr[i] = i
 
-arr[0] = random.randint(1,99)
+arr[0] = random.randint(1, 99)
 random.shuffle(arr)
-print (arr)
+print(arr)
 
-x = 4950 #1+2+.....+99 = 4950
+x = 0
+for i in range(0, 100):
+    x = x + i
+
 total = 0
-for i in range (len(arr)):
+for i in range(len(arr)):
     total += arr[i]
-print ("Duplicate Num: ", total - x)
+print("Duplicate Num: ", total - x)
